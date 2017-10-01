41f147dfd04dbc6bd8b51b273ac468640c76134e
Ed J <mohawk2@users.noreply.github.com>
2017-10-01 21:47:34 +0100

    serve graphql from trivial schema

--- lib/MyApp.pm
+++ lib/MyApp.pm
@@ -1,14 +1,31 @@
 package MyApp;
 use Dancer2;
+use GraphQL::Schema;
+use GraphQL::Type::Object;
+use GraphQL::Type::Scalar qw($String);
+use GraphQL::Execution;
 
 our $VERSION = '0.1';
 
+set serializer => 'JSON'; # allows request->parameters get JSON, must send_as HTML if mean that
+
 get '/' => sub {
-    template 'index' => { 'title' => 'MyApp' };
+    send_as html => template 'index' => { 'title' => 'MyApp' };
+};
+
+my $schema = GraphQL::Schema->new(query => GraphQL::Type::Object->new(
+  name => 'QueryRoot',
+  fields => {
+    helloWorld => { type => $String, resolve => sub { 'Hello world!' } },
+  },
+));
+
+post '/graphql' => sub {
+  send_as JSON => GraphQL::Execution->execute($schema, body_parameters->{query});
 };
 
 get '/graphql' => sub {
-  template 'graphiql' => {
+  send_as html => template 'graphiql' => {
     title => 'GraphiQL',
     graphiql_version => '0.11.2',
     queryString => safe_serialise(params->{query}),
