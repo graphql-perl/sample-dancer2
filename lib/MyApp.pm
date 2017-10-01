e860761eb88a2047b405d8dcc977da1cd587ce4c
Ed J <mohawk2@users.noreply.github.com>
2017-10-01 20:47:16 +0100

    get graphiql showing

--- lib/MyApp.pm
+++ lib/MyApp.pm
@@ -7,4 +7,25 @@ get '/' => sub {
     template 'index' => { 'title' => 'MyApp' };
 };
 
+get '/graphql' => sub {
+  template 'graphiql' => {
+    title => 'GraphiQL',
+    graphiql_version => '0.11.2',
+    queryString => safe_serialise(params->{query}),
+    operationName => safe_serialise(params->{operationName}),
+    resultString => safe_serialise(params->{result}),
+    variablesString => safe_serialise(params->{variables}),
+  };
+};
+
+my $JSON = JSON::MaybeXS->new->allow_nonref;
+# spelled rite
+sub safe_serialise {
+  my ($data) = @_;
+  return 'undefined' if !$data;
+  my $json = $JSON->encode($data);
+  $json =~ s#/#\\/#g;
+  $json;
+}
+
 true;
